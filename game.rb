0eed103dd7a3ccc7c7b8b88966bd27048dd25992
App Academy Student <student@appacademy.io>
2016-11-17 18:00:18 -0800

    add save feature

--- game.rb
+++ game.rb
@@ -1,8 +1,14 @@
 require_relative "board"
 require_relative "tile"
 require 'byebug'
+require 'yaml'
 
 class Game
+
+  def self.load(filename)
+    saved_state = File.read(filename)
+    Game.new(YAML::load(saved_state))
+  end
 
   def initialize(board)
     @board = board
@@ -13,14 +19,27 @@ class Game
     string.scan(/\d/).map(&:to_i)
   end
 
+  def save
+    puts "Enter a file name"
+    saved_file_name = gets.chomp
+
+    saved_game = @board.to_yaml
+
+    File.open(saved_file_name, 'w') do |file|
+      file.write(saved_game)
+    end
+  end
+
   def play_turn
     puts "Enter a position (ex: 3, 4)"
     pos = parse_pos(gets.chomp)
-    puts "Reveal (r) or Flag (f)?"
+    puts "Reveal (r) or Flag (f) or Save (s)?"
     act = gets.chomp.downcase
     if act == "f"
       @board[pos].flag
       @board.render
+    elsif act == "s"
+      save
     else
       if !@board[pos].flagged && @board[pos].bomb?
         @board.reveal_all
